3fab6ca996d8c9c538420ad74f3cb943980baa23
maneeshamuvva <muvvamaneesha@gmail.com>
2022-07-02 23:35:51 -0400

    currenttimedate

--- show_time.py
+++ show_time.py
@@ -1,4 +1,15 @@
-from datetime import datetime
+import calendar
+from datetime import date
+import datetime
 
-time=datetime.now().time()
-print("current time:", time)
+e = datetime.datetime.now()
+
+print ("Current date and time = %s" % e)
+
+print ("Today's date:  = %s/%s/%s" % (e.day, e.month, e.year))
+today = date.today()
+print(calendar.day_name[today.weekday()])
+
+
+
+
